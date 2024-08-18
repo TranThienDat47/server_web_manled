6c87fa3e632d8d4debd2e6981ddeae421d0b24de
TranThienDat47 <tranthiendat220102@gmail.com>
2024-08-18 12:00:09 +0700

    test

--- config/db/index.js
+++ config/db/index.js
@@ -1,36 +1,36 @@
 import mongoose from 'mongoose';
 
-const url = 'mongodb://localhost:27017/web-flim';
-const options = {
-   useNewUrlParser: true,
-   useUnifiedTopology: true,
-   family: 4,
-};
-
-const connectDB = async () => {
-   try {
-      await mongoose.connect(url, options);
-      console.log('MongoDB connected');
-   } catch (error) {
-      console.log(error);
-      process.exit(1);
-   }
-};
+// const url = 'mongodb://localhost:27017/web-flim';
+// const options = {
+//    useNewUrlParser: true,
+//    useUnifiedTopology: true,
+//    family: 4,
+// };
 
 // const connectDB = async () => {
 //    try {
-//       await mongoose.connect(
-//          `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.rdlf0om.mongodb.net/test`,
-//          {
-//             useNewUrlParser: true,
-//             useUnifiedTopology: true,
-//          },
-//       );
+//       await mongoose.connect(url, options);
 //       console.log('MongoDB connected');
 //    } catch (error) {
 //       console.log(error);
 //       process.exit(1);
 //    }
 // };
+
+const connectDB = async () => {
+   try {
+      await mongoose.connect(
+         `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.rdlf0om.mongodb.net/test`,
+         {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+         },
+      );
+      console.log('MongoDB connected');
+   } catch (error) {
+      console.log(error);
+      process.exit(1);
+   }
+};
 
 export default { connectDB };
